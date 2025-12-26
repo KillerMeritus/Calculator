0df322d588c8557705141c9b4b9a0135376d0864
KillerMeritus <senv9098@gmail.com>
2025-12-26 20:08:42 +0530

    fixed oppration and added AC,backspace functionalities

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let Input = document.querySelector('#input-box')
 
 let btns = document.querySelectorAll('.num');
 let eql = document.querySelector('.eql')
-
+let oldInput = 0
 
 // Number input
 btns.forEach((btn)=>{
@@ -16,28 +16,49 @@ btns.forEach((btn)=>{
 
 //Math Operations
 
-// addition
 let Opprs = document.querySelectorAll('.oppr')
 Opprs.forEach((Oppr)=>{
     Oppr.addEventListener('click',()=>{
         console.log("add clicked")
-        let oldInput = Input.value
+        oldInput = Input.value
         console.log(Input.value)
-        Input.value = ""
-        let newInput = Input.value
+        Input.value = 0
 
-        eql.addEventListener('click',()=>{
-            Input.value = operation(Number(oldInput),Number(newInput),String(Oppr.id))
-        })
         
+        eql.addEventListener('click',()=>{
+            Input.value = operation(Number(oldInput),Number(Input.value),String(Oppr.id))
+        })
         
     })
 })
 
-console.log(oper(5,3,))
+// AC button feature
+let AC = document.querySelector('.ac')
+AC.addEventListener('click',()=>{
+    location.reload();
+})
 
 
-function operation(a = Number, b = Number, operation){
+// backspace feature
+let Bs = document.querySelector('.bs')
+Bs.addEventListener('click',()=>{
+    Input.value = Input.value.slice(0,-1)
+})
+
+// Decimal feature 
+let Decimal = document.querySelector('.decimal')
+Decimal.addEventListener('click',()=>{
+    console.log("dot")
+    Input.value = (Input.value + ".")
+})
+
+// Sign change
+let Sign = document.querySelector('.sign')
+Sign.addEventListener('click',()=>{
+    Input.value = Number(Input.value) * -1
+})
+
+function operation(a, b, operation){
     if(operation === "Add"){
         console.log(a+b)
         return (a + b);
